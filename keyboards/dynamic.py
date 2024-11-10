1524d5544dc20a013ac0920dd0d4fdbac20617c9
souls1o <161530718+souls1o@users.noreply.github.com>
2024-11-10 15:24:24 -0800

    Update dynamic.py

--- keyboards/dynamic.py
+++ keyboards/dynamic.py
@@ -1,1 +1,23 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+def create_main_menu_keyboard():
+    keyboard = [
+        [InlineKeyboardButton("🚀 Menu", callback_data="menu")],
+        [InlineKeyboardButton("👤 Account", callback_data="account")]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+    
+def create_menu_keyboard(menu_items):
+    keyboard = [
+        [InlineKeyboardButton("🚀 Menu", callback_data="menu")],
+        [InlineKeyboardButton("👤 Account", callback_data="account")]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+    
+def create_account_keyboard():
+    keyboard = [
+        [InlineKeyboardButton("📂 Orders", callback_data="orders")],
+        [InlineKeyboardButton("💳 Add Funds", callback_data="deposit")],
+        [InlineKeyboardButton("⬅️ Back", callback_data="main_menu")]
+    ]
+    return InlineKeyboardMarkup(keyboard)
