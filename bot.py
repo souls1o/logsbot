3e11426343fc903e2523a15a974e7e0a72140e1b
souls1o <161530718+souls1o@users.noreply.github.com>
2024-11-10 16:08:41 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,22 +11,12 @@ test_connection()
 
 parse_mode = "MarkdownV2"
 
-# MAIN COMMANDS #
-async def start(update: Update, context: CallbackContext) -> None:
-    chat_id = get_chat_id(update)
-    
-
-# MAIN FUNCTIONS #
-def get_chat_id(update: Update) -> int:
-    return update.message.chat_id if update.message else update.callback_query.message.chat_id
-
-def filter_text(text: str):
-    return text.replace('_', '\\_').replace('-', '\\-').replace('.', '\\.').replace('!', '\\!').replace('(', '\\(').replace(')', '\\)').replace('[', '\\[').replace(']', '\\]').replace('=', '\\=').replace('<', '\\<').replace('>', '\\>')
-
 def main() -> None:
     app = Application.builder().token(os.environ["TELEGRAM_BOT_TOKEN"]).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling(poll_interval=5)
+    
+    application.add_handler(start.get_handler())
+    
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
