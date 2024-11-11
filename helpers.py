4614dc761cc1773f2c61400233ca1336acde2c71
souls1o <161530718+souls1o@users.noreply.github.com>
2024-11-10 18:20:49 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import random
 import string
+from telegram import Update
 
 def get_chat_id(update: Update) -> int:
     return update.message.chat_id if update.message else update.callback_query.message.chat_id
