0e77b2bbea9b6b117f10524485c8fc0d889e0246
souls1o <161530718+souls1o@users.noreply.github.com>
2024-11-10 16:25:47 -0800

    Update display.py

--- screens/display.py
+++ screens/display.py
@@ -9,3 +9,17 @@ async def show_main_menu(update, context):
     
     text = f"🔱 *Welcome to __POSEIDON__, _{user}_!* 🔱\n\n* ℹ️ Poseidon is the #1 and only bot on the market where you can purchase HQ logs seamlessly using various cryptocurrencies such as BTC, ETH, and LTC. To get started, add funds from the account menu and search through our menu to find logs that fit your needs.*\n\n📞 *Support: @fwsouls*"
     await context.bot.send_message(chat_id, text, parse_mode, reply_markup)
+    
+async def show_menu(update, context):
+    chat_id = update.effective_chat.id
+    reply_markup = create_menu_keyboard()
+    
+    text = "🚀 *Menu*"
+    await context.bot.send_message(chat_id, text, parse_mode, reply_markup)
+    
+async def show_account(update, context):
+    chat_id = update.effective_chat.id
+    reply_markup = create_menu_keyboard()
+    
+    text = "👤 *Account*"
+    await context.bot.send_message(chat_id, text, parse_mode, reply_markup)
