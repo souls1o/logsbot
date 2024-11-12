63ade875cf436b61b72f7689a1eb643a39eb8b70
souls1o <161530718+souls1o@users.noreply.github.com>
2024-11-12 13:29:39 -0800

    Update display.py

--- screens/display.py
+++ screens/display.py
@@ -10,13 +10,13 @@ async def show_main_menu(update, context):
     user = update.effective_user.first_name
     reply_markup = create_main_menu_keyboard()
     
-    text = f"🔱 *Welcome to __POSEIDON__, _{user}_\\!* 🔱\n\n> *ℹ️ Poseidon is the \\#1 and only bot on the market where you can purchase HQ logs seamlessly using various cryptocurrencies such as BTC, ETH, and LTC\\. To get started, add funds from the account menu and search through our menu to find logs that fit your needs\\.*\n\n📞 *Support: @fwsouls*"
+    text = f"🔱 *Welcome to __Poseidon Logs__, _{user}_\\!* 🔱\n\n> *ℹ️ Poseidon is the \\#1 and only bot on the market where you can purchase HQ logs seamlessly using various cryptocurrencies such as BTC, ETH, and LTC\\. To get started, add funds from the account menu and search through our menu to find logs that fit your needs\\.*\n\n🔗 *Channel: t\\.me/sheloveosamaa*\n📞 *Support: @fwsouls*"
     await context.bot.send_message(chat_id, text, parse_mode, reply_markup=reply_markup)
     
 async def show_menu(update, context):
     chat_id = update.effective_chat.id
     
-    # create_log("$15-$20 Balance", 5, "Subway", "Food", "account")
+    # create_log("$15-$20 Balance", 5, 3, "Subway", "Food", "account")
     
     logs_count = sum(len(log.get("logs", [])) for log in get_all_logs())
     reply_markup = create_menu_keyboard(logs_count, 0)
@@ -45,7 +45,7 @@ async def show_account_logs(update, context):
     for product, info in product_info.items():
         emoji = get_emoji(info["category"])
         price = info["price"]
-        product_text = f"> \\[{emoji}\\] *{product}*\n> 💲*_{price:.2f}_*"
+        product_text = f"> \\[{emoji}\\] *{product}* | 💲*_{price:.2f}_*\\+"
         
         product_lines.append(product_text)
         
