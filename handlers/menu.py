f67280e06b3ded55f95c0f8f289b48a71a1e7127
souls1o <161530718+souls1o@users.noreply.github.com>
2024-11-13 12:36:22 -0800

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,12 +1,14 @@
 from telegram import Update
 from telegram.ext import CallbackQueryHandler, ContextTypes
-from screens.display import show_menu, show_account, show_account_logs
+from screens.display import show_main_menu, show_menu, show_account, show_account_logs
 
 async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
     
-    if query.data == "menu":
+    if query.data == "main_menu":
+        await show_main_menu(update, context)
+    elif query.data == "menu":
         await show_menu(update, context)
     elif query.data == "account":
         await show_account(update, context)
