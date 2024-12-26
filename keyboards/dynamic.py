bfa6539d65d5e352e65f2e69213786c0370c34a4
souls1o <161530718+souls1o@users.noreply.github.com>
2024-12-26 15:49:01 -0800

    Update dynamic.py

--- keyboards/dynamic.py
+++ keyboards/dynamic.py
@@ -11,7 +11,7 @@ def create_main_menu_keyboard():
     
 def create_menu_keyboard(logs_count, banks_count):
     keyboard = [
-        [InlineKeyboardButton(f"📲 Account Logs ({logs_count})", callback_data="logs_account")],
+        [InlineKeyboardButton(f"👥 Account Logs ({logs_count})", callback_data="logs_account")],
         [InlineKeyboardButton(f"🏦 Bank Logs ({banks_count})", callback_data="logs_bank")],
         [InlineKeyboardButton("⬅️ Back", callback_data="main_menu")]
     ]
