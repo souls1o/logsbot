1ce7c686571742d28c3f19e6d429caa57614bf44
souls1o <161530718+souls1o@users.noreply.github.com>
2024-12-26 14:54:04 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -25,3 +25,18 @@ def get_emoji(category):
     }
     
     return category_emojis.get(category, "❓")
+    
+def emojify(number):
+    digit_to_emoji = {
+        '0': '0️⃣',
+        '1': '1️⃣',
+        '2': '2️⃣',
+        '3': '3️⃣',
+        '4': '4️⃣',
+        '5': '5️⃣',
+        '6': '6️⃣',
+        '7': '7️⃣',
+        '8': '8️⃣',
+        '9': '9️⃣',
+    }
+    return ''.join(digit_to_emoji[digit] for digit in str(number))
