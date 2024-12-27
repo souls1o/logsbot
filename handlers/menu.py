5be1f0c323a090ea2ee0abda429e8e0983a4e509
souls1o <161530718+souls1o@users.noreply.github.com>
2024-12-26 16:16:46 -0800

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import CallbackQueryHandler, ContextTypes
-from screens.display import show_main_menu, show_menu, show_account, show_orders, show_order, show_account_logs
+from screens.display import show_main_menu, show_menu, show_account, show_orders, show_order, show_account_logs, show_options
 
 async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
@@ -16,6 +16,9 @@ async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await show_orders(update, context)
     elif query.data == "logs_account":
         await show_account_logs(update, context)
+    elif query.data.startswith("product_"):
+        product = query.data.split("_", 1)[1]
+        await show_options(update, context, product)
     elif query.data.startswith("order_"):
         order_id = query.data.split("_", 1)[1]
         await show_order(update, context, order_id)
