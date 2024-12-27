b6759d97b79acde935a92759fec1a9d051178f83
souls1o <161530718+souls1o@users.noreply.github.com>
2024-12-26 17:37:00 -0800

    Update display.py

--- screens/display.py
+++ screens/display.py
@@ -89,7 +89,7 @@ async def show_options(update, context, product):
             log_texts.append(f"> {cat_emoji} *$__{log_price:.2f}__ | {log_name}*\n> #️⃣ _{log_id}_")
             
     logs_display = "\n\n".join(log_texts)
-    text = escape_markdown(f"{cat_emoji} *{product}\n\n{logs_display}")
+    text = escape_markdown(f"{cat_emoji} *{product}*\n\n{logs_display}")
     await context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text, parse_mode=parse_mode)
     
 async def show_account(update, context):
