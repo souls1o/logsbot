7b002b918731a44e603e2cbee23382c979865c26
souls1o <161530718+souls1o@users.noreply.github.com>
2025-01-17 20:37:54 -0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,7 @@
+import requests
 from flask import Flask, request
 from threading import Thread
+from db import get_user, update_user, get_transaction, create_transaction, update_transaction
 
 app = Flask(__name__)
 
@@ -10,9 +12,27 @@ def index():
 @app.route('/callback', methods=['POST'])
 def callback():
     data = request.json
+    user_id = data["data"]["user_id"]
+    value = data["value"]
+    currency = data["currency"]
+    txid = data["input_transaction_hash"]
+    confimations = data["confirmations"]
     
-    print(data)
-
+    transactions = get_all_transactions()
+    
+    transaction = next((tx for tx in transactions if tx["info"]["txid"] == txid), None)
+    if transaction:
+        if transaction["status"] != "confirmed" and confirmations >= 1:
+            user = get_user(user_id)
+            user["balances"][currency] += value/100000000
+            user["transactions"].append(transaction["transaction_id"])
+            update_user(user_id, user)
+            
+            transaction["status"] = "confirmed"
+            update_transaction(transaction["transaction_id"], transaction)
+    else:
+        create_transaction(user_id, value, currency, txid)
+    
 def run():
   app.run(host='0.0.0.0', port=8080)
 
