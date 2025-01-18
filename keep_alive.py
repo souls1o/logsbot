8568d2a17b1b7356f97e360841cee6bfe9157e8e
souls1o <161530718+souls1o@users.noreply.github.com>
2025-01-17 21:49:44 -0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -24,16 +24,30 @@ def callback():
     if transaction:
         if transaction["status"] != "confirmed" and confirmations >= 1:
             user = get_user(user_id)
-            user["balances"][currency] += value/100000000
+            amount = value/100000000
+            user["balances"][currency] += amount
             user["transactions"].append(transaction["transaction_id"])
             update_user(user_id, user)
             
             transaction["status"] = "confirmed"
             update_transaction(transaction["transaction_id"], transaction)
+            
+            curr_up = currency.upper()
+            message = (
+              f"✅ *Payment Confirmed*\n\n"
+              f"_Your payment of *{amount} {curr_up}* has successfully confirmed._"
+            )
+            
+            payload = {
+              "chat_id": user_id,
+              "text": message,
+              "parse_mode": "MarkdownV2"
+            }
+            
+            requests.post("https://api.telegram.org/bot7845815456:AAE3qpHwyUj21VX32_k4XiwoajLVGsxxdBc/sendMessage", json=payload)
     else:
         create_transaction(user_id, value, currency, txid)
         
-    print(data)
     return {"status": "received"}, 200
     
 def run():
