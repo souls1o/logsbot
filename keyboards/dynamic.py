d3294506b27451751691a757fc1c5bc54716a0b6
souls1o <161530718+souls1o@users.noreply.github.com>
2025-01-27 19:07:27 -0800

    Update dynamic.py

--- keyboards/dynamic.py
+++ keyboards/dynamic.py
@@ -96,6 +96,13 @@ def create_options_keyboard(log_ids, product_data):
     
     return InlineKeyboardMarkup(keyboard)
     
+def create_option_keyboard(log_id):
+    keyboard = [
+        [InlineKeyboardButton("🛒 Add to Cart", callback_data=f"add_cart_{log_id}")]
+        [InlineKeyboardButton("⬅️ Back", callback_data="depo")]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+    
 def create_addr_keyboard():
     keyboard = [
         [InlineKeyboardButton("⬅️ Back", callback_data="depo")]
