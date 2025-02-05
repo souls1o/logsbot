3a34c0fa1ebd875bc7613e75b3887c152ac2cf9d
souls1o <161530718+souls1o@users.noreply.github.com>
2025-02-05 15:54:49 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 from db import test_connection
 from telegram.ext import Application
-from handlers import start, menu, adminstats, create_log
+from handlers import start, menu, adminstats, create_log, ref
 from keep_alive import keep_alive
 
 keep_alive()
@@ -13,6 +13,7 @@ def main() -> None:
     app = Application.builder().token(os.environ["TELEGRAM_BOT_TOKEN"]).build()
     
     app.add_handler(start.get_handler())
+    app.add_handler(ref.get_handler())
     app.add_handler(adminstats.get_handler())
     app.add_handler(create_log.get_handler())
     app.add_handler(menu.get_handler())
