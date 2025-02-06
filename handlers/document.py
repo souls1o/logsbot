c7c8223b1f3df4ac08836c781fedc3471e2911a3
souls1o <161530718+souls1o@users.noreply.github.com>
2025-02-06 13:57:02 -0800

    Update document.py

--- handlers/document.py
+++ handlers/document.py
@@ -1,5 +1,6 @@
 from telegram import Update
-from telegram.ext import MessageHandler, CallbackContext, filters
+from telegram.ext import MessageHandler, CallbackContext
+from telegram.ext.filters import Document, FileExtension
 from db import get_log, update_log
 from helpers import escape_markdown
 
@@ -30,4 +31,4 @@ log)
         await update.message.reply_text("Successfully updated stock")
 
 def get_handler():
-    return MessageHandler(filters.Document, handle_document)
+    return MessageHandler(Document & FileExtension("txt"), handle_document)
