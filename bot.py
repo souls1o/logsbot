726d36c3ade21b4246834e1db8e4c82d08d345d6
souls1o <161530718+souls1o@users.noreply.github.com>
2025-02-06 21:43:19 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 from db import test_connection
 from telegram.ext import Application
-from handlers import start, menu, adminstats, create_log, add_stock, document, ref
+from handlers import start, menu, adminstats, refstats, create_log, add_stock, document, ref
 from keep_alive import keep_alive
 
 keep_alive()
@@ -17,6 +17,7 @@ def main() -> None:
     app.add_handler(add_stock.get_handler())
     app.add_handler(document.get_handler())
     app.add_handler(adminstats.get_handler())
+    app.add_handler(refstats.get_handler())
     app.add_handler(create_log.get_handler())
     app.add_handler(menu.get_handler())
     
