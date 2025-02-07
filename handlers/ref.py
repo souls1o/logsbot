2751b2e4ae3940cae807c801abef5dc8e8cb499e
souls1o <161530718+souls1o@users.noreply.github.com>
2025-02-06 16:27:00 -0800

    Update ref.py

--- handlers/ref.py
+++ handlers/ref.py
@@ -1,14 +1,20 @@
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 from screens.display import show_main_menu
-from db import update_user
+from db import get_user, update_user
 
 async def ref(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data.clear()
     
-    code = ""
-    if context.args:
-        code = context.args[0]
+    if not context.args:
+        return
+        
+    user = get_user(update.effective_user.id)
+        
+    code = context.args[0]
+    if code == "test":
+        user["ref"] = 7434895838
+        update_user(update.effective_user.id, user)
     
     await show_main_menu(update, context)
 
