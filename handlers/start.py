1937f0028ce43695e0470a55a60947c8646eae16
souls1o <161530718+souls1o@users.noreply.github.com>
2025-02-06 17:24:37 -0800

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 from screens.display import show_main_menu
+from db import get_user, update_user
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data.clear()
@@ -8,9 +9,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.args:
         user_id = update.effective_user.id
         user = get_user(user_id)
-        if user["ref"] == None:
+        if not user:
+            user = create_user(user_id)
             if context.args[0] == "test":
-                user["ref"] = 7434895838
+                user["ref"] = 7863772639
                 update_user(user_id, user)
     
     await show_main_menu(update, context)
