2324c0a0744e74d92a882276c4dda7c8b05bf97a
souls1o <161530718+souls1o@users.noreply.github.com>
2025-02-06 21:51:11 -0800

    Update refstats.py

--- handlers/refstats.py
+++ handlers/refstats.py
@@ -3,7 +3,7 @@ from telegram.ext import CommandHandler, ContextTypes
 from screens.display import show_ref_stats
 
 async def refstats(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await show_red_stats(update, context)
+    await show_ref_stats(update, context)
 
 def get_handler():
     return CommandHandler('refstats', refstats)
